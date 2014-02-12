9d8753ea8c75494b452e9dc441c3414628da897a
Chris Franklin <cfranklin@supercoderbrothers.com>
2014-02-11 23:12:09 -0800

    Adding a nav bar to the static pages.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Agile::Application.routes.draw do
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
+  get "static_pages/features"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
